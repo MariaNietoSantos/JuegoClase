7a6fe5250a7823394ce76170989c9396277d36d8
MariaNietoSantos <marianietosantos@gmail.com>
2023-01-10 12:59:08 +0100

    10.1.2023 13:00h

--- Assets/Script/Personaje.cs
+++ Assets/Script/Personaje.cs
@@ -22,4 +22,8 @@ public class Personaje : MonoBehaviour
             rigidbody.AddForce(Vector2.up * salto);
         }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
 }
