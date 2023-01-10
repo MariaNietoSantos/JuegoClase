69a264317eef3cabcca250dc2c193b2af3d884b5
MariaNietoSantos <marianietosantos@gmail.com>
2023-01-10 12:43:47 +0100

    Saltar

---
+++ Assets/Script/Personaje.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rigidbody;
+    [SerializeField] Animator animator;
+    [SerializeField] float salto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidbody.AddForce(Vector2.up * salto);
+        }
+    }
+}
