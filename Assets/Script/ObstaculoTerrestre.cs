f52e13121d53a3a7d4bd98a1c5858000b301396a
Anbersan <122355364+Anbersan@users.noreply.github.com>
2023-01-12 14:56:03 +0100

    movimiento kk

--- Assets/Script/ObstaculoTerrestre.cs
+++ Assets/Script/ObstaculoTerrestre.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 public class ObstaculoTerrestre : MonoBehaviour
 {
     [SerializeField] Camera mainCamera;
-    [SerializeField] float velocidad;
-    [SerializeField] Vector3 posicionI;
-    [SerializeField] Vector3 posicionF;
+    [SerializeField] float velocidad = 4;
+    [SerializeField] Vector2 posicionI;
+    [SerializeField] Vector2 posicionF;
+    [SerializeField] Vector2 posicionIkk;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.main;
+        posicionI = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        posicionF = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+
+        posicionIkk = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x > posicionI.x)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * velocidad);
+        }
+        else
+        {
+            transform.position = posicionIkk;
+        }
     }
 }
