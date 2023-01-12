5a7a0036ad4938be0d0c5fe1aa0ca5439f5a54c3
Anbersan <122355364+Anbersan@users.noreply.github.com>
2023-01-12 14:26:56 +0100

    empezando codigo

---
+++ Assets/Script/ObstaculoTerrestre.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaculoTerrestre : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+    [SerializeField] float velocidad;
+    [SerializeField] Vector3 posicionI;
+    [SerializeField] Vector3 posicionF;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
