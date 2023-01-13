1c4a17a03743088b8b8b321b3998925c6035d54c
Anbersan <122355364+Anbersan@users.noreply.github.com>
2023-01-13 08:53:26 +0100

    aumento de velocidad

--- Assets/Script/ObstaculoTerrestre.cs
+++ Assets/Script/ObstaculoTerrestre.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class ObstaculoTerrestre : MonoBehaviour
 {
     [SerializeField] Camera mainCamera;
-    [SerializeField] float velocidad = 4;
+    [SerializeField] float velocidad;
     [SerializeField] Vector2 posicionI;
     [SerializeField] Vector2 posicionF;
     [SerializeField] Vector2 posicionIkk;
     // Start is called before the first frame update
     void Start()
     {
+        velocidad = 4;
         mainCamera = Camera.main;
         posicionI = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
         posicionF = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
@@ -29,6 +30,12 @@ public class ObstaculoTerrestre : MonoBehaviour
         else
         {
             transform.position = posicionIkk;
+            velocidad = velocidad + 2;
+        }
+
+        if (velocidad = 202)
+        {
+            velocidad = velocidad - 2;
         }
     }
 }
